c7ca12f93d06e948655a187db154a5ee33ad035b
cliff-codes <simplecodes2580@gmail.com>
2025-06-11 22:20:44 +0000

    booleans

---
+++ boolean.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+int main(){
+
+    bool isStudent = false;
+
+    char name[50] = "";
+
+    if(isStudent){
+        printf("You are a student!");
+    }else{
+        printf("You are not a student");
+    }
+
+    //checking for name;
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) -1 ] = '\0';
+
+    if(strlen(name) == 0){
+        printf("You did not enter your name");
+    }else{
+        printf("Hello, %s", name);
+    }
+
+
+    return 0;
+}
