00a04f9be77a4da930bd079c7b663e87b62a4fe8
cliff-codes <simplecodes2580@gmail.com>
2025-06-11 22:08:27 +0000

    if statements

--- IfStatements.c
+++ IfStatements.c
@@ -4,13 +4,20 @@
 
 int main() {
 
-    int age = 21;
+    int age = 31;
 
-    if (age >= 18)
+    if (age < 18)
     {
         /* code */
+        printf("You are a minor");
+    }else if (age >= 18 && age <= 30)
+    {
+        /* code */
+        printf("You are a young adult");
+    }else{
         printf("You are an adult");
     }
+    
     
 
     return 0;
