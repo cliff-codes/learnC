a2909154f2657a74168ed7362a5a828349951a56
cliff-codes <simplecodes2580@gmail.com>
2025-06-11 23:15:35 +0000

    whileloop

---
+++ whileLoop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    int timer = 10;
+
+    while (1 <= timer)
+    {
+        printf("Countdown_Timer: %d\n", timer);
+        timer--;
+    }
+    
+    return 0;
+}
